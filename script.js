98595f888aa25fa1c9b704bc8541662da4fda758
sidfakenews986 <bisonsicolo@hotmail.com>
2025-08-29 04:04:11 +0100

    Add event listener to change color based on input

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // Event listener to change color based on input
+const colorInput = document.getElementById('colorInput');
+const box = document.getElementById('colorBox');
+
+colorInput.addEventListener('input', function() {
+    box.style.backgroundColor = colorInput.value;
+});
